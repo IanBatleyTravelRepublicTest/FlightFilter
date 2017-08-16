a4493933069d51f24f4344d70dcdbd81263879d9
IanBatleyTravelRepublicTest <Ian@ANIMAL>
2017-08-16 21:38:06 +0200

    Added dynamic rules engine to allow for easier addition and removal of rules at a later date

---
+++ FlightFilterApp/Extensions/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightFilterApp.Extensions
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> GetFilteredItems<T>(this IEnumerable<T> listToFilter, IEnumerable<Func<T, bool>> filters)
+        {
+            //This is a bit cryptic - but, basically, return anything from the list that matchs all of the filters. 
+            return listToFilter.Where(item => !filters.Any(filterTest => filterTest(item)));
+        }
+    }
+}
--- FlightFilterApp/Program.cs
+++ FlightFilterApp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using FlightFilterApp.Builders;
 using FlightFilterApp.Entities;
+using FlightFilterApp.Extensions;
 
 namespace FlightFilterApp
 {
@@ -19,42 +20,41 @@ namespace FlightFilterApp
             //Flight builder internally stores state around dates - this means it should probably be treated as a singleton so that dates do not change if run over date barriers
             _flightBuilder = new FlightBuilder();
 
-            GetFilteredFlights();
+            var filters = GetFilters();
+
+            GetFilteredFlights(filters);
             
         }
 
-        public static void GetFilteredFlights()
+        private static List<Func<Flight, bool>> GetFilters()
         {
-            var flights = _flightBuilder.GetFlights();
+            DateTime nowTime = DateTime.Now;
 
-            var nowTime = DateTime.Now;
+            var filters = new List<Func<Flight, bool>>()
+            {
+                //1. Depart before the current date/time.
+                flight => flight.Segments.Any(seg => seg.DepartureDate < nowTime), 
+                //2. Have a segment with an arrival date before the departure date.
+                flight => flight.Segments.Any(seg => seg.ArrivalDate < seg.DepartureDate), 
+                //3. Spend more than 2 hours on the ground. i.e those with a total gap of over two hours between the arrival date of one segment and the departure date of the next.
+                flight => flight.Segments.Any(seg => !CheckGroundStopTimeBetweenSegments(flight.Segments.OrderBy(s => s.DepartureDate).ToList(), seg)) 
+            };
+
+            return filters;
+        }
+
+
+        public static void GetFilteredFlights(List<Func<Flight, bool>> filters)
+        {
+            if (filters == null || !filters.Any()) return;
+
+            var flights = _flightBuilder.GetFlights();            
 
             if(flights.Any(f => !f.Segments.Any())) throw new InvalidOperationException("Could not operate - flights found with no segments");
 
-            /*
-            1. Depart before the current date/time.
-            2. Have a segment with an arrival date before the departure date.
-            3. Spend more than 2 hours on the ground. i.e those with a total gap of over two hours between the arrival
-                date of one segment and the departure date of the next. */
-
-
-            var filteredFlights =
-                flights.Where(flight =>
-                    {
-                        var orderedSegments = flight.Segments.OrderBy(s => s.DepartureDate).ToList();
-
-                        return flight.Segments.All(fs => fs.DepartureDate > nowTime) &&
-                               flight.Segments.Any(fs => fs.ArrivalDate > fs.DepartureDate) &&
-                               flight.Segments.All(seg => CheckGroundStopTimeBetweenSegments(orderedSegments, seg));
-                    }).ToList();
-
-            var jsonSerialiser = new JavaScriptSerializer();
-            var json = jsonSerialiser.Serialize(filteredFlights);
-
-            Console.WriteLine(json);
-
-            File.WriteAllText(@"..\..\result\result.json", json);
-
+            var filteredFlights = flights.GetFilteredItems(filters);
+            
+            PersistResults(filteredFlights);
         }
 
         private static bool CheckGroundStopTimeBetweenSegments(List<Segment> orderedSegments , Segment currentSegment)
@@ -70,6 +70,16 @@ namespace FlightFilterApp
             TimeSpan timeSpentOnGround = nextSegment.DepartureDate - currentSegment.ArrivalDate;
 
             return timeSpentOnGround.TotalHours < 2;
-        }        
+        }
+
+        private static void PersistResults(IEnumerable<Flight> filteredFlights)
+        {
+            var jsonSerialiser = new JavaScriptSerializer();
+            var json = jsonSerialiser.Serialize(filteredFlights);
+
+            File.WriteAllText(@"..\..\result\result.json", json);
+
+            Console.WriteLine(json);
+        }
     }
 }
