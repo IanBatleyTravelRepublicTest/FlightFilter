3ef958064e5e796834dfbce74b5d6dabafabf130
IanBatleyTravelRepublicTest <Ian@ANIMAL>
2017-08-16 19:53:57 +0200

    Added example code and split into seperate files

---
+++ FlightFilterApp/Entities/Flight.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace FlightFilterApp.Entities
+{
+    public class Flight
+    {
+        public IList<Segment> Segments { get; set; }
+    }
+}
---
+++ FlightFilterApp/Entities/Segment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FlightFilterApp.Entities
+{
+    public class Segment
+    {
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+    }
+}
